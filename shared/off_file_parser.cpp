f7cf774ab64f3f3133b42672385b02e26357fd77
filiprejmus <anarchiaonline@gmail.com>
2022-06-28 17:07:33 +0200

    Homework 3 init

--- shared/off_file_parser.cpp
+++ shared/off_file_parser.cpp
@@ -48,6 +48,42 @@ std::tuple<std::vector<Eigen::Vector3f>, std::vector<Eigen::Vector3f>, std::vect
             off_file.close();
             return {points, normals, {min_x, min_y, min_z}, {max_x, max_y, max_z}};
         }
+        else if (header_keyword == "NOFF")
+        {
+            int vertex_count, face_count, edge_count;
+            off_file >> vertex_count >> face_count >> edge_count;
+            std::vector<Eigen::Vector3f> points;
+            std::vector<Eigen::Vector3f> normals;
+            float max_x, max_y, max_z = -10000;
+            float min_x, min_y, min_z = 10000;
+            for (int i = 0; i < vertex_count; ++i)
+            {
+                float x, y, z;
+                float nx, ny, nz;
+                off_file >> x >> y >> z >> nx >> ny >> nz;
+                if (x > max_x)
+                    max_x = x;
+                if (y > max_y)
+                    max_y = y;
+                if (z > max_z)
+                    max_z = z;
+                if (x < min_x)
+                    min_x = x;
+                if (y < min_y)
+                    min_y = y;
+                if (z < min_z)
+                    min_z = z;
+
+                Eigen::Vector3f point;
+                Eigen::Vector3f normal;
+                point << x, y, z;
+                normal << nx, ny, nz;
+                points.push_back(point);
+                normals.push_back(normal);
+            }
+            off_file.close();
+            return {points, normals, {min_x, min_y, min_z}, {max_x, max_y, max_z}};
+        }
         else
         {
             off_file.close();
